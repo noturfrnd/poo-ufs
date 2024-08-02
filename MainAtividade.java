c88cae3030ffd28f7a29a635100482d7bc3ae2e0
noturfrnd <jpaneto@dcomp.ufs.br>
2024-08-02 00:01:11 -0300

    menu midgame

--- MainAtividade.java
+++ MainAtividade.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class MainAtividade {
@@ -12,7 +13,37 @@ public class MainAtividade {
 	public static ArrayList<Produto> estoque = new ArrayList<>();
 
 	public static void main(String[] args) {
-		// TODO Fa�a aqui o seu programa
+		System.out.println(" ----------------------- Bem vinto a loja! -----------------------");
+		String escolha = loadMenu();
+		while (!escolha.trim().equals("10")) {
+			switch (escolha) {
+				case "1":
+					System.out.println("Digite o nome do categoria: ");
+					cadastrarCategorias(scanner.nextLine());
+					escolha = loadMenu();
+				case "2":
+					System.out.println("Digite o nome do subcategoria: ");
+					String subcategoria = scanner.nextLine();
+					System.out.println("Digite o nome da Categoria Base: ");
+					String categoriaBase = scanner.nextLine();
+					cadastrarSubcategoria(subcategoria, categoriaBase);
+					escolha = loadMenu();
+				case "3":
+					System.out.println("Digite o codigo do produto: ");
+					int codigoProduto = scanner.nextInt();
+					System.out.println("Digite o nome do produto: ");
+					scanner.nextLine();
+					String produto = scanner.nextLine();
+					System.out.println("Digite a quantidade do produto: ");
+					int quantidadeProduto = scanner.nextInt();
+					scanner.nextLine();
+					System.out.println("Digite a categoria do produto: ");
+					String categoria = scanner.nextLine();
+					cadastrarProduto(codigoProduto,produto,categoria,quantidadeProduto);
+					escolha = loadMenu();
+				case "4":
+			}
+		}
 
 	}
 
@@ -25,8 +56,26 @@ public class MainAtividade {
 		return null;
 	}
 
-	public static void loadMenu (){
-		System.out.println("Bem vinto a loja! Digite o numero da ação que deseja:");
+	public static Subcategoria buscarSubCategoria(String busca){
+		for (Subcategoria cat: listaSubCategorias){
+			if(cat.getNomeSubcategoria().equals(busca)){
+				return cat;
+			}
+		}
+		return null;
+	}
+
+	public static ArrayList<String> listarSubcategorias(){
+		ArrayList<String> subcategorias = new ArrayList<>();
+		for (Subcategoria subc: listaSubCategorias){
+			subcategorias.add(subc.getNomeSubcategoria());
+		}
+		return subcategorias;
+	}
+
+	public static String loadMenu (){
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Digite o numero da ação que deseja:");
 		System.out.println("1 - Cadastrar categoria.");
 		System.out.println("2 - Cadastrar subcategoria.");
 		System.out.println("3 - Cadastrar produto.");
@@ -37,10 +86,23 @@ public class MainAtividade {
 		System.out.println("8 - Consultar estoque.");
 		System.out.println("9 - Repor estoque.");
 		System.out.println("10 - Sair.");
+		return scanner.nextLine();
 	}
 
-	public static void cadastrarCategorias(Categoria categoria, Pessoa pessoa){
-		if (!listaCategorias.contains(categoria) || buscarCategoria(categoria.getNomeCategoria()) != null){
+	public static void cadastrarCategorias(String categoriaNome){
+		Categoria categoria = new Categoria();
+		if (buscarCategoria(categoriaNome) == null){
+			categoria.setNomeCategoria(categoriaNome);
+			System.out.println("Agora adicione as sub-categorias que deseja separadas por ',' essas sao as categorias disponíveis: ");
+			System.out.println(listarSubcategorias().toString());
+			String subcategorias = scanner.nextLine();
+			String[] subcategoriasArray = subcategorias.split(",");
+			ArrayList<Subcategoria> subcategoriasLista = new ArrayList<>();
+			for (String subcategoria: subcategoriasArray){
+				subcategoriasLista.add(buscarSubCategoria(subcategoria));
+			}
+			categoria.setListaSubcategorias(subcategoriasLista);
+			System.out.println("Sub-categorias adicionadas: " + categoria.getListaSubcategorias().toString());
 			listaCategorias.add(categoria);
 			return;
 		}
@@ -49,18 +111,23 @@ public class MainAtividade {
 
 	public static void cadastrarSubcategoria(String nomeCategoria, String nomeSubcategoria){
 		if(buscarCategoria(nomeCategoria) == null){
-			System.out.println("Categoria base não existe");
+			System.out.println("Categoria base não existe.");
 			return;
 		}
-		for(Subcategoria sub: listaSubCategorias){
-			if(sub.getNomeSubcategoria().equals(nomeSubcategoria)){
-				System.out.println("Essa subcategoria ja existe");
-				return;
-			}
+		if(buscarSubCategoria(nomeSubcategoria) != null){
+			System.out.println("Sub-categoria ja existe.");
 		}
 		Subcategoria subcategoria = new Subcategoria();
 		subcategoria.setNomeSubcategoria(nomeSubcategoria);
 		subcategoria.setCategoriaBase(buscarCategoria(nomeCategoria));
+		for (Categoria cat: listaCategorias){
+			if (cat.getNomeCategoria().equals(subcategoria.getNomeCategoria())){
+				List<Subcategoria> subcategoriasLista = cat.getListaSubcategorias();
+				subcategoriasLista.add(subcategoria);
+				cat.setListaSubcategorias(subcategoriasLista);
+			}
+		}
+		System.out.println("Sub-categoria cadastrada com sucesso.");
 	}
 
 	public static void cadastrarProduto(int codigo, String nome, String categoria, int qtdEstoque){
